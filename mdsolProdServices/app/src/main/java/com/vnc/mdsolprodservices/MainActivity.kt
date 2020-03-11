5542b2072bd8a9b44bdb15179777509c6da0d780
vin20784 <vin20784@gmail.com>
2020-03-11 23:53:28 +0530

    medi app layout and recycler view

--- mdsolProdServices/app/src/main/java/com/vnc/mdsolprodservices/MainActivity.kt
+++ mdsolProdServices/app/src/main/java/com/vnc/mdsolprodservices/MainActivity.kt
@@ -1,41 +1,35 @@
 package com.vnc.mdsolprodservices
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 import kotlinx.android.synthetic.main.activity_main.*
-
-
-
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-    }
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+        val dataList = ArrayList<MovieModel>()
+        dataList.add(MovieModel("eCOA ", 1999,"Patient Cloud",4.8))
+        dataList.add(MovieModel("eConsent", 2003,"Patient Cloud", 4.3))
+        dataList.add(MovieModel("Virtual Trials", 2003 ,"Patient Cloud",4.1))
+        dataList.add(MovieModel("eTMF", 2021 ,"Trial Management",0.0))
+        dataList.add(MovieModel("CTMS", 2008, "Trial Management" ,4.2))
+        dataList.add(MovieModel("CSA", 2008, "Trial Management" ,4.2))
+        dataList.add(MovieModel("RBQM", 2010, "Trial Management" ,4.4))
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        //        pass the values to RvAdapter
+        val rvAdapter = RcViewAdapter(this,dataList)
+//        set the recyclerView to the adapter
+        recyclerView.adapter = rvAdapter;
+
     }
 }
