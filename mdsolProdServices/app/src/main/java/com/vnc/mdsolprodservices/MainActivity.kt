7c9f71398dbc748b7e3bff1ff834385d7d4aafca
vin20784 <vin20784@gmail.com>
2020-03-11 19:41:49 +0530

    splash screen added

--- mdsolProdServices/app/src/main/java/com/vnc/mdsolprodservices/MainActivity.kt
+++ mdsolProdServices/app/src/main/java/com/vnc/mdsolprodservices/MainActivity.kt
@@ -5,8 +5,6 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
